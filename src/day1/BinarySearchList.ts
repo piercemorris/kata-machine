5b76faa1051b7456fa2f4ea4edda6726dcbb3e0e
Pierce Morris <pierce.morris1998@gmail.com>
2023-12-07 15:36:35 +0000

    bs

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,19 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+  let low = 0
+  let high = haystack.length
 
+  do {
+    const i = Math.floor(low + (high-low) / 2)
+    const v = haystack[i]
+
+    if (v === needle) return true
+
+    if (v < needle) {
+      low = i + 1
+    } else {
+      high = i
+    }
+  } while (low < high)
+
+  return false
 }
