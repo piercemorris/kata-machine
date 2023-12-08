d540786b02e9fa8012add453d8174b05d8661d35
Pierce Morris <pierce.morris1998@gmail.com>
2023-12-08 10:55:39 +0000

    bubblesort

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,3 +1,13 @@
 export default function bubble_sort(arr: number[]): void {
 
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      let curr = arr[j]
+      let next = arr[j+1]
+      if (curr > next) {
+        arr[j] = next
+        arr[j+1] = curr
+      }
+    }
+  }
 }
